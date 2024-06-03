e4eb862f8fce7ec48b8635ca4aa2ae3d9f665232
ismaildgn16 <ismaildogan.egm@gmail.com>
2024-06-03 16:59:00 +0300

    first commit

---
+++ script.js
@@ -0,0 +1,29 @@
+const arrows = document.querySelectorAll(".arrow");
+const movieLists = document.querySelectorAll(".movie-list");
+
+arrows.forEach((arrow, i) => {
+  const widthRadio = Math.floor(window.innerWidth / 300);
+  let clickCounter = 0;
+  const imageItem = movieLists[i].querySelectorAll("img").length;
+  arrow.addEventListener("click", function () {
+    clickCounter++;
+    if (imageItem - (4 + clickCounter) + (4 - widthRadio) >= 0) {
+      const computedStyle = window.getComputedStyle(movieLists[i]);
+      const matrix = new WebKitCSSMatrix(computedStyle.transform);
+      movieLists[i].style.transform = `translateX(${matrix.m41 - 300}px)`;
+    } else {
+      movieLists[i].style.transform = "translateX(0)";
+      clickCounter = 0;
+    }
+  });
+});
+
+/* Dark mode */
+const ball = document.querySelector(".toggle-ball");
+const items = document.querySelectorAll(
+  ".container,.navbar,.sidebar,.sidebar i,.toggle,.toggle-ball,.movie-list-filter select,.movie-list-title"
+);
+
+ball.addEventListener("click", function () {
+  items.forEach((item) => item.classList.toggle("active"));
+});
